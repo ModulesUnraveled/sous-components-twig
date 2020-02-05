07a0576d7cb47c8e55d13faf6d4206ec655f5675
Brian Lewis <brian@modulesunraveled.com>
2020-02-05 17:20:17 -0600

    Moved card files into 'basic' folder.

--- 02-molecules/card/cards.stories.js
+++ 02-molecules/card/cards.stories.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import card from './card.twig';
+import card from './basic/card.twig';
 
-import cardData from './card.yml';
-import cardBgData from './card-bg.yml';
+import cardData from './basic/card.yml';
+import cardBgData from './basic/card-bg.yml';
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Molecules/Cards' };
 
-export const cardExample = () => <div dangerouslySetInnerHTML={{ __html: card(cardData) }} />;
+export const cardExample = () => (
+  <div dangerouslySetInnerHTML={{ __html: card(cardData) }} />
+);
 export const cardWithBackground = () => (
-  <div dangerouslySetInnerHTML={{ __html: card({ ...cardData, ...cardBgData }) }} />);
+  <div
+    dangerouslySetInnerHTML={{ __html: card({ ...cardData, ...cardBgData }) }}
+  />
+);
