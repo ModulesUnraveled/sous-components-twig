dd08c93c1775b92d362e38f7fc6dd85f78489ca7
Brian Lewis <brian@modulesunraveled.com>
2020-02-05 17:08:31 -0600

    Updated button file locations and examples.

--- 01-atoms/buttons/buttons.stories.js
+++ 01-atoms/buttons/buttons.stories.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
-import button from './twig/button.twig';
+import button from './button.twig';
 
-import buttonData from './twig/button.yml';
-import buttonAltData from './twig/button-alt.yml';
-import buttonAlt2Data from './twig/button-alt2.yml';
+import buttonData from './button.yml';
+import buttonAltData from './button-alt.yml';
+import buttonAlt2Data from './button-alt2.yml';
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Atoms/Buttons' };
 
-export const Default = () => <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />;
-export const alt = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />;
-export const alt2 = () => <div dangerouslySetInnerHTML={{ __html: button(buttonAlt2Data) }} />;
+export const All = () => (
+  <>
+    <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: button(buttonAlt2Data) }} />
+  </>
+);
+export const Default = () => (
+  <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />
+);
+export const alt = () => (
+  <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />
+);
+export const alt2 = () => (
+  <div dangerouslySetInnerHTML={{ __html: button(buttonAlt2Data) }} />
+);
