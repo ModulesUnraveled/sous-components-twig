41eb073c91fcc6f959e6975e2462365ae4a1ee64
Brian Lewis <brian@modulesunraveled.com>
2020-02-06 11:00:28 -0600

    Crated an event cards grid.

--- 03-organisms/grid/grids.stories.js
+++ 03-organisms/grid/grids.stories.js
@@ -5,14 +5,30 @@ import grid from './grid.twig';
 import gridData from './grid.yml';
 import gridCardData from './grid-cards.yml';
 import gridCtaData from './grid-ctas.yml';
+import gridEventCardData from './grid-event-cards.yml';
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Organisms/Grids' };
 
-export const defaultGrid = () => <div dangerouslySetInnerHTML={{ __html: grid(gridData) }} />;
+export const defaultGrid = () => (
+  <div dangerouslySetInnerHTML={{ __html: grid(gridData) }} />
+);
 export const cardGrid = () => (
-  <div dangerouslySetInnerHTML={{ __html: grid({ ...gridData, ...gridCardData }) }} />);
+  <div
+    dangerouslySetInnerHTML={{ __html: grid({ ...gridData, ...gridCardData }) }}
+  />
+);
 export const ctaGrid = () => (
-  <div dangerouslySetInnerHTML={{ __html: grid({ ...gridData, ...gridCtaData }) }} />);
+  <div
+    dangerouslySetInnerHTML={{ __html: grid({ ...gridData, ...gridCtaData }) }}
+  />
+);
+export const eventCardGrid = () => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html: grid({ ...gridData, ...gridEventCardData }),
+    }}
+  />
+);
