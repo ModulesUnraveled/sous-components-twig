3f8603f41ff9cf1ab6e896d3bce4bfb31c8b2334
Brian Lewis <brian@modulesunraveled.com>
2020-02-07 06:16:38 -0600

    Created news-card.

--- 02-molecules/cards/cards.stories.js
+++ 02-molecules/cards/cards.stories.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import cardTwig from './basic/card.twig';
 import eventCardTwig from './event/event-card.twig';
+import newsCardTwig from './news/news-card.twig';
 
 import cardData from './basic/card.yml';
 import cardBgData from './basic/card-bg.yml';
 import eventCardData from './event/event-card.yml';
+import newsCardData from './news/news-card.yml';
 
 /**
  * Storybook Definition.
@@ -24,4 +26,7 @@ export const cardWithBackground = () => (
 );
 export const eventCard = () => (
   <div dangerouslySetInnerHTML={{ __html: eventCardTwig(eventCardData) }} />
+);
+export const newsCard = () => (
+  <div dangerouslySetInnerHTML={{ __html: newsCardTwig(newsCardData) }} />
 );
