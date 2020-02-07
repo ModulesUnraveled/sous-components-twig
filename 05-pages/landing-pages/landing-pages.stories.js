cafacc3ee2611df58d3437d636301abf8bcfb594
Brian Lewis <brian@modulesunraveled.com>
2020-02-07 13:41:35 -0600

    Cleaned up homepage story.

--- 05-pages/landing-pages/landing-pages.stories.js
+++ 05-pages/landing-pages/landing-pages.stories.js
@@ -8,7 +8,6 @@ import '../../02-molecules/menus/main-menu/main-menu';
 import home from './home.twig';
 
 import mainMenuData from '../../02-molecules/menus/main-menu/main-menu.yml';
-import breadcrumbData from '../../02-molecules/menus/breadcrumbs/breadcrumbs.yml';
 import socialMenuData from '../../02-molecules/menus/social/social-menu.yml';
 import footerMenuData from '../../02-molecules/menus/inline/inline-menu.yml';
 
@@ -17,22 +16,20 @@ import footerMenuData from '../../02-molecules/menus/inline/inline-menu.yml';
  */
 hrefTo('Pages/Content Types', 'Article').then((url) => {
   // TODO: Can't figure out how to link pages with hrefTo and storiesOf.
-  storiesOf('Pages/Landing Pages', module)
-    .add('Home', () => {
-      useEffect(() => Drupal.attachBehaviors(), []);
-      return (
-        <div dangerouslySetInnerHTML={{
+  storiesOf('Pages/Landing Pages', module).add('Home', () => {
+    useEffect(() => Drupal.attachBehaviors(), []);
+    return (
+      <div
+        dangerouslySetInnerHTML={{
           __html: home({
             page_layout_modifier: 'contained',
             ...mainMenuData,
-            ...breadcrumbData,
             ...socialMenuData,
             ...footerMenuData,
             card_link_url: url,
-            card__link__text: 'Click here',
           }),
         }}
-        />
-      );
-    });
+      />
+    );
+  });
 });
