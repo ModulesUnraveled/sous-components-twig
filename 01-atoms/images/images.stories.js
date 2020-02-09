8a01ca4852847459dd7cd89ef0d6dd3b3b1bbb49
Brian Lewis <brian@modulesunraveled.com>
2020-02-09 13:12:10 -0600

    Crated background-image component.

--- 01-atoms/images/images.stories.js
+++ 01-atoms/images/images.stories.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { useEffect } from '@storybook/client-api';
 
 import image from './image/responsive-image.twig';
 import figure from './image/figure.twig';
 import iconTwig from './icons/icons.twig';
+import bgImageTwig from './background-image/background-image.twig';
 
 import imageData from './image/image.yml';
 import figureData from './image/figure.yml';
 import iconData from './icons/icons.yml';
+import bgImageData from './background-image/background-image.yml';
+
+import './background-image/background-image';
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Atoms/Images' };
 
-export const images = () => <div dangerouslySetInnerHTML={{ __html: image(imageData) }} />;
-export const figures = () => <div dangerouslySetInnerHTML={{ __html: figure(figureData) }} />;
-export const icons = () => <div dangerouslySetInnerHTML={{ __html: iconTwig(iconData) }} />;
+export const images = () => (
+  <div dangerouslySetInnerHTML={{ __html: image(imageData) }} />
+);
+export const figures = () => (
+  <div dangerouslySetInnerHTML={{ __html: figure(figureData) }} />
+);
+export const icons = () => (
+  <div dangerouslySetInnerHTML={{ __html: iconTwig(iconData) }} />
+);
+export const backgroundImage = () => {
+  useEffect(() => Drupal.attachBehaviors(), []);
+  return <div dangerouslySetInnerHTML={{ __html: bgImageTwig(bgImageData) }} />;
+};
